285fb4b85560dfd01c936c2f716fae17f834c66a
arrase <arrase@gmail.com>
2017-04-25 21:10:59 +0200

    install orbot if not present

--- app/src/main/java/io/github/arrase/onioncat/fragments/SettingsFragment.java
+++ app/src/main/java/io/github/arrase/onioncat/fragments/SettingsFragment.java
@@ -1,12 +1,17 @@
 package io.github.arrase.onioncat.fragments;
 
 
+import android.content.Context;
 import android.os.Bundle;
+import android.preference.Preference;
 import android.preference.PreferenceFragment;
 
+import info.guardianproject.netcipher.proxy.OrbotHelper;
 import io.github.arrase.onioncat.R;
+import io.github.arrase.onioncat.helpers.CheckDependenciesHelper;
 
 public class SettingsFragment extends PreferenceFragment {
+    private Context mContext;
 
     public SettingsFragment() {
 
@@ -16,5 +21,26 @@ public class SettingsFragment extends PreferenceFragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.ocat_settings);
+
+        // Orbot
+        Preference installOrbot = (Preference) findPreference(getString(R.string.pref_orbot_installed));
+
+        if (CheckDependenciesHelper.checkOrbot(mContext)) {
+            installOrbot.setSummary(R.string.orbot_is_installed);
+        } else {
+            installOrbot.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+                @Override
+                public boolean onPreferenceClick(Preference preference) {
+                    startActivity(OrbotHelper.getOrbotInstallIntent(mContext));
+                    return true;
+                }
+            });
+        }
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        mContext = context;
     }
 }
