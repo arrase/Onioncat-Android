40f2124911ec4ece4524aa9fc06583ce2533a40a
arrase <arrase@gmail.com>
2017-04-25 19:27:09 +0200

    first commit

--- build_ocat.sh
+++ build_ocat.sh
@@ -1,17 +1,19 @@
 #!/usr/bin/env bash
 
 
-ANDROID_NDK=$(cat local.properties |grep ndk.dir|awk -F '=' '{print $2}')
+export ANDROID_NDK=$(cat local.properties |grep ndk.dir|awk -F '=' '{print $2}')
 
 # Download
-[ -d external ] || mkdir external
-[ -d external/onioncat ] && rm -rf external/onioncat
+[ -d external ] && rm -rf external
+mkdir external
 wget https://www.cypherpunk.at/ocat/download/Source/current/onioncat-0.2.2.r571.tar.gz
-tar xzf onioncat-0.2.2.r571.tar.gz -O external/onioncat
+tar xzf onioncat-0.2.2.r571.tar.gz -C external/
 rm onioncat-0.2.2.r571.tar.gz
 
 # Build
-cd external/onioncat
+cd external/onioncat-0.2.2.r571
 wget https://www.cypherpunk.at/onioncat_trac/export/HEAD/trunk/android/android_configure
 sh android_configure
 make
+
+cp src/ocat ../../app/src/main/res/raw/
