ef07ad491af37f381a8792fb63a7cbb3e9415098
arrase <arrase@gmail.com>
2017-04-28 22:47:32 +0200

    refactor

--- app/src/main/java/io/github/arrase/onioncat/constants/OcatConstant.java
+++ app/src/main/java/io/github/arrase/onioncat/constants/OcatConstant.java
@@ -4,7 +4,6 @@ public class OcatConstant {
     public static final String START_ORBOT = "io.github.arrase.onioncat.START_ORBOT";
     public static final String START_ORBOT_END = "io.github.arrase.onioncat.START_ORBOT_END";
     public static final String START_OCAT = "io.github.arrase.onioncat.START_OCAT";
-    public static final String STOP_OCAT = "io.github.arrase.onioncat.STOP_OCAT";
     public final static String INTENT_ACTION_REQUEST_HIDDEN_SERVICE = "org.torproject.android.REQUEST_HS_PORT";
     public final static String BINARY_DIRECTORY = "bin";
 
